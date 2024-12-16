6972f95a856902880956e1f1a58b816edec6d296
JJ <luzginjj@gmail.com>
2024-12-16 17:28:37 +0200

    Frontend start

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,28 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500,
+      },
+    },
+  },  
+  
+  defaultNetwork: "running",
+
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+
+    running: {
+      url: "http://localhost:8545",
+      chainId: 1337,
+    },
+  },
 };
 
 export default config;
